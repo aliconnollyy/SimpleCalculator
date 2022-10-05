459274a94c52fee0ff57d9ad87daa92fea03465c
Alex <connoa11@tcd.ie>
2022-10-05 18:57:10 +0100

    added operator precedence function and added function to apply operator to operands

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -1,6 +1,45 @@
 import java.util.Scanner;
 public class SimpleCalculator{
 
+
+    // Returns true if 'op2' has higher
+    // or same precedence as 'op1',
+    // otherwise returns false.
+    public static boolean hasPrecedence(char op1, char op2)
+    {
+        if (op2 == '(' || op2 == ')')
+        {
+            return false;
+        }
+        if ((op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-'))
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    // A utility method to apply an
+    // operator 'op' on operands 'a'
+    // and 'b'. Return the result.
+    public static int applyOp(char operator, int operand1, int operand2)
+    {
+        switch (operator)
+        {
+        case '+':
+            return operand1 + operand2;
+        case '-':
+            return operand1 - operand2;
+        case '*':
+            return operand1 * operand2;
+
+        default:
+        }
+        return 0;
+    }
+
     public static void main(String[] args)
     {
         String userInput;
